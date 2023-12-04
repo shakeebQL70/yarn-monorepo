a33d6373d2e0be21dbbad2818fe28e245f142647
Shakeeb Arsalan <shakeebadab50@gmail.com>
2023-12-04 18:51:47 +0530

    only changes on super app 4

--- apps/super/src/App.tsx
+++ apps/super/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
       changes again on super app
 
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, fuga. Corporis quod expedita officia non provident. Fugiat magnam possimus id minima eius ut, sint dicta magni aspernatur totam deleniti at!
+  
       <Suspense fallback="Loading...">
         <OPMS />
         <UI />
